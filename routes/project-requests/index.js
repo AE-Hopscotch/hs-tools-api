e931023f0e77dd737b2c6fbda17cd179bf4bda15
AE-Hopscotch <98666048+AE-Hopscotch@users.noreply.github.com>
2022-04-08 11:03:18 -0700

    use a webhook instead of email

--- routes/project-requests/index.js
+++ routes/project-requests/index.js
@@ -1,4 +1,3 @@
-const { sendMail } = require('../../custom/email.js')
 const express = require('express')
 const router = express.Router()
 const Joi = require('joi')
@@ -95,24 +94,28 @@ router.post('/', async (req, res) => {
       { 'defaultText': response.secret, name: 'key', type: 'text' }
     ]
   })
+  await axios({
+    method: 'POST',
+    url: process.env.PROJECT_REQUEST_WEBHOOK,
+    data: {
+      embeds: [{
+        color: 0x521256,
+        author: { name: 'New Project Request', iconURL: `https://s3.amazonaws.com/hopscotch-cover-images/production/${project.uuid}.png` },
+        title: '',
+        description: `You have a new project request for [${project.title}](https://c.gethopscotch.com/p/${project.uuid})`,
+        fields: [
+          { name: 'UUID', value: project.uuid, inline: true },
+          { name: 'Author', value: project.user?.nickname, inline: true },
+          { name: 'Reason', value: response.edit_reason, inline: true },
+          { name: '\u1D00\u1D04\u1D1B\u026A\u1D0F\u0274', value: `[Reject...](https://hs-tools-api.up.railway.app/form/${rejectForm})`, inline: true },
+          { name: '\u200B', value: `[Mark Received](https://hs-tools-api.up.railway.app/project-requests/${response.key}/received?key=${response.secret})`, inline: true },
+          { name: '\u200B'.repeat(8), value: `[Complete...](https://hs-tools-api.up.railway.app/form/${completeForm})`, inline: true }
+        ],
+        timestamp: new Date().toISOString()
+      }]
+    }
+  })
 
-  await sendMail(process.env.NODEMAILER_USER, 'HS Tools API Notification',
-    `<!DOCTYPE html><html lang="en" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml"><head><title></title>
-      <meta charset="utf-8"/><meta content="width=device-width, initial-scale=1.0" name="viewport"/><style>*{box-sizing: border-box;}body{margin: 0;padding: 0;}a[x-apple-data-detectors]{color: inherit !important;text-decoration: inherit !important;}#MessageViewBody a{color: inherit;text-decoration: none;}p{line-height: inherit}@media (max-width:520px){.icons-inner{text-align: center;}.icons-inner td{margin: 0 auto;}.row-content{width: 100% !important;}.stack .column{width: 100%;display: block;}}</style></head>
-      <body style="background-color: #FFFFFF; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;"><table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF;" width="100%"><tbody><tr><td><table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%"><tbody><tr><td><table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500"><tbody><tr><td class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%"><table border="0" cellpadding="0" cellspacing="0" class="heading_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%"><tr><td style="width:100%;text-align:center;"><h1 style="margin: 0; color: #555555; font-size: 23px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; line-height: 120%; text-align: center; direction: ltr; font-weight: normal; letter-spacing: normal; margin-top: 0; margin-bottom: 0;"><strong>You have a new Project Modding Request</strong></h1></td></tr></table><table border="0" cellpadding="10" cellspacing="0" class="text_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%"><tr><td><div style="font-family: Arial, sans-serif"><div style="font-size: 12px; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; mso-line-height-alt: 14.399999999999999px; color: #555555; line-height: 1.2;"><p style="margin: 0; font-size: 14px; text-align: center;"><span style="font-size:14px;">Hi. This is an automated message to let you know that there is a new project modding request pending.</span></p><p style="margin: 0; font-size: 14px; text-align: center; mso-line-height-alt: 14.399999999999999px;">&nbsp;</p><p style="margin: 0; font-size: 14px; text-align: center;"><span style="font-size:14px;">
-      Project Title: ${project.title}
-      </span></p><p style="margin: 0; font-size: 14px; text-align: center;"><span style="font-size:14px;">
-      Project UUID: ${project.uuid}
-      </span></p><p style="margin: 0; font-size: 14px; text-align: center;"><span style="font-size:14px;">
-      Editing Reason: ${response.edit_reason}
-      </span></p></div></div></td></tr></table></td></tr></tbody></table></td></tr></tbody></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%"><tbody><tr><td><table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500"><tbody><tr><td class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="33.333333333333336%"><table border="0" cellpadding="0" cellspacing="0" class="button_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%"><tr><td style="text-align:center;padding-top:15px;padding-right:10px;padding-bottom:15px;padding-left:10px;"><div align="center">
-      <a href="https://hs-tools-api.up.railway.app/form/${rejectForm}" style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#b31010;border-radius:4px;width:auto;border-top:1px solid #b31010;border-right:1px solid #b31010;border-bottom:1px solid #b31010;border-left:1px solid #b31010;padding-top:5px;padding-bottom:5px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;text-align:center;mso-border-alt:none;word-break:keep-all;" target="_blank"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="font-size: 16px; line-height: 2; word-break: break-word; mso-line-height-alt: 32px;">Reject</span></span></a></div></td></tr></table></td><td class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="33.333333333333336%"><table border="0" cellpadding="0" cellspacing="0" class="button_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%"><tr><td style="text-align:center;padding-top:15px;padding-right:10px;padding-bottom:15px;padding-left:10px;"><div align="center">
-      <a href="https://hs-tools-api.up.railway.app/project-requests/${response.key}/received?key=${response.secret}" style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#3AAEE0;border-radius:4px;width:auto;border-top:1px solid #3AAEE0;border-right:1px solid #3AAEE0;border-bottom:1px solid #3AAEE0;border-left:1px solid #3AAEE0;padding-top:5px;padding-bottom:5px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;text-align:center;mso-border-alt:none;word-break:keep-all;" target="_blank"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="font-size: 16px; line-height: 2; word-break: break-word; mso-line-height-alt: 32px;">Received</span></span></a></div></td></tr></table></td><td class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="33.333333333333336%"><table border="0" cellpadding="0" cellspacing="0" class="button_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%"><tr><td style="text-align:center;padding-top:15px;padding-right:10px;padding-bottom:15px;padding-left:10px;"><div align="center">
-      <a href="https://hs-tools-api.up.railway.app/form/${completeForm}" style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#197b23;border-radius:4px;width:auto;border-top:1px solid #197b23;border-right:1px solid #197b23;border-bottom:1px solid #197b23;border-left:1px solid #197b23;padding-top:5px;padding-bottom:5px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;text-align:center;mso-border-alt:none;word-break:keep-all;" target="_blank"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="font-size: 16px; line-height: 2; word-break: break-word; mso-line-height-alt: 32px;">Complete</span></span></a></div></td></tr></table></td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table></body></html>
-    `
-  ).catch(
-    e => console.log(e)
-  )
   delete response.secret // Exclude secret from response
   res.send(response)
 })
