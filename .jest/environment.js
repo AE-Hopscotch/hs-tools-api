2024600705f58debda63a61407714ed173f796a6
AE-Hopscotch <98666048+AE-Hopscotch@users.noreply.github.com>
2022-06-20 03:28:04 -0700

    Add test env vars

--- .jest/environment.js
+++ .jest/environment.js
@@ -18,4 +18,12 @@ global.deta = () => ({
   }
 })
 
+process.env.PROJECT_KEY = 'ProjectKeySample'
 process.env.ADMIN_API_KEY = 'AdminTest'
+process.env.CORS_ORIGINS = 'https://ae-hopscotch.github.io'
+process.env.NODEMAILER_USER = 'email@example.com'
+process.env.NODEMAILER_CLIENTID = 'ThisIsFake'
+process.env.NODEMAILER_CLIENTSECRET = 'ThisIsNotReal'
+process.env.NODEMAILER_REFRESHTOKEN = 'ThisIsAToken'
+process.env.MOD_REQ_STATUS = 'OPEN'
+process.env.PROJECT_REQUEST_WEBHOOK = 'webhook-url://test-environment-url'
