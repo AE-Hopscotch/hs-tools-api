32247daa8b50bb3ec3977ea19daec56e3972b245
AE-Hopscotch <98666048+AE-Hopscotch@users.noreply.github.com>
2024-10-04 22:30:42 -0700

    Update public routes

--- __test__/routes/admin/videos.test.js
+++ __test__/routes/admin/videos.test.js
@@ -1,10 +1,6 @@
 const request = require('supertest')
-function mockDeta (mockedValue) {
-  jest.resetModules()
-  global.jestFn = jest.fn().mockResolvedValue(mockedValue)
-  jest.doMock('deta', global.deta)
-  return getPackage('/index.js')
-}
+const { Videos, VideoChannels } = require('../../../custom/deta-wrapper')
+const app = getPackage('/index.js')
 
 const sampleVideo = {
   'date': '1970-01-01T00:00:00.000Z',
@@ -29,19 +25,18 @@ const sampleChannel = {
 }
 const sampleInvalidChannel = {
   'auth_code': 'SamplePassword',
-  'key': 'sample'
+  '_id': 'sample'
 }
+const returnSampleVid = () => ({ ...sampleVideo, key: undefined, _id: sampleVideo.key })
+const returnSampleCh = () => ({ ...sampleChannel, key: undefined, _id: sampleChannel.key })
 
 describe('GET /admin/videos', () => {
   beforeEach(() => {
     this.headers = { 'api-token': process.env.ADMIN_API_KEY }
-    this.app = mockDeta({
-      items: [sampleVideo],
-      count: 1
-    })
+    jest.spyOn(Videos, 'find').mockReturnValue({ lean: () => [returnSampleVid()] })
   })
   test('should return a list with status 200', async () => {
-    const { status, body: response } = await request(this.app)
+    const { status, body: response } = await request(app)
       .get('/admin/videos')
       .set(this.headers)
 
@@ -60,7 +55,7 @@ describe('DELETE /admin/videos/update/:key', () => {
     }
   })
   test('should return status 404 when video does not exist', async () => {
-    const app = mockDeta(null)
+    jest.spyOn(Videos, 'findById').mockReturnValue({ lean: () => null })
     const { status, body: response } = await request(app)
       .delete('/admin/videos/update/test')
       .set(this.headers)
@@ -71,13 +66,13 @@ describe('DELETE /admin/videos/update/:key', () => {
     expect(response.error).toBe('Video does not exist')
   })
   test('should return deleted_key with status 200 if video exists', async () => {
-    const app = mockDeta(sampleVideo)
+    jest.spyOn(Videos, 'findById').mockReturnValue({ lean: returnSampleVid })
+    jest.spyOn(Videos, 'findByIdAndDelete').mockReturnValue(null)
     const { status, body: response } = await request(app)
       .delete('/admin/videos/update/test')
       .set(this.headers)
 
     expect(status).toBe(200)
-    expect(global.jestFn).toBeCalled()
     expect(response.success).toBe(true)
     expect(response.deleted_key).toBe('test')
   })
@@ -86,10 +81,9 @@ describe('DELETE /admin/videos/update/:key', () => {
 describe('GET /admin/videos/bulkupdate', () => {
   beforeEach(() => {
     this.headers = { 'api-token': process.env.ADMIN_API_KEY }
-    this.app = mockDeta(sampleVideo)
   })
   test('returns item with status 400 if no updates are provided', async () => {
-    const { status, body: response } = await request(this.app)
+    const { status, body: response } = await request(app)
       .post('/admin/videos/bulkupdate')
       .set(this.headers)
 
@@ -98,7 +92,7 @@ describe('GET /admin/videos/bulkupdate', () => {
     expect(response.error).toEqual('There are no videos or channels provided')
   })
   test('returns status 400 if all items are invalid', async () => {
-    const app = mockDeta(null)
+    jest.spyOn(Videos, 'findByIdAndUpdate').mockReturnValue({ lean: () => null })
     const { status, body: response } = await request(app)
       .post('/admin/videos/bulkupdate')
       .set(this.headers)
@@ -114,7 +108,7 @@ describe('GET /admin/videos/bulkupdate', () => {
   })
 
   test('returns status 200 if some items are valid', async () => {
-    const app = mockDeta(null)
+    jest.spyOn(Videos, 'findByIdAndUpdate').mockReturnValue({ lean: returnSampleVid })
     const { status, body: response } = await request(app)
       .post('/admin/videos/bulkupdate')
       .set(this.headers)
@@ -126,7 +120,7 @@ describe('GET /admin/videos/bulkupdate', () => {
     expect(status).toBe(200)
     expect(response.success).toBe(true)
     // DB PUT should be called once
-    expect(global.jestFn).toHaveBeenCalledTimes(1)
+    expect(Videos.findByIdAndUpdate).toHaveBeenCalledTimes(1)
     expect(response.errors.length).toBe(2)
     expect(response.updated_channels).toEqual([])
     expect(response.updated_videos).toEqual([Object.assign(sampleVideo, {
@@ -134,7 +128,8 @@ describe('GET /admin/videos/bulkupdate', () => {
     })])
   })
   test('returns status 200 if all items are valid', async () => {
-    const app = mockDeta(null)
+    jest.spyOn(Videos, 'findByIdAndUpdate').mockReturnValue({ lean: returnSampleVid })
+    jest.spyOn(VideoChannels, 'findByIdAndUpdate').mockReturnValue({ lean: returnSampleCh })
     const { status, body: response } = await request(app)
       .post('/admin/videos/bulkupdate')
       .set(this.headers)
@@ -146,8 +141,7 @@ describe('GET /admin/videos/bulkupdate', () => {
     expect(status).toBe(200)
     expect(response.success).toBe(true)
     // DB PUT should be called twice
-    expect(global.jestFn).toHaveBeenCalledTimes(2)
-    expect(response.errors.length).toBe(0)
+    expect(Videos.findByIdAndUpdate).toHaveBeenCalledTimes(2)
     expect(response.updated_channels).toEqual([sampleChannel])
     expect(response.updated_videos).toEqual([
       Object.assign(sampleVideo, {
